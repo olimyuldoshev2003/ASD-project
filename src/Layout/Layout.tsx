2634b8470ea56282a9f987f07bc7b4bdd7836845
Olim Yuldoshev <oyuldoshev39@gmail.com>
2023-12-01 17:54:36 +0500

    Fixed some mistakes

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -42,134 +42,141 @@ function Layout() {
 
   return (
     <div className="p-[30px_100px] sm:p-[30px_10px] bg-[#fff] dark:bg-[#333333]">
-      <header className="header flex justify-between items-center">
-        <div className="block_1">
-          <Link to="/">
-            <img
-              src={logoHeader}
-              alt=""
-              className="w-[80px] h-[80px] rounded-full"
-            />
-          </Link>
-        </div>
-        <ul className="block_2 md:flex md:items-center md:gap-5 sm:hidden">
-          <li>
-            <Link
-              to="/"
-              className={
-                location.pathname === "/"
-                  ? " dark:text-[#b6b6b6] text-[16px] font-[700] underline"
-                  : "text-[16px] font-[700] text-[#000] dark:text-[#fff] dark:hover:text-[#b6b6b6] hover:underline"
-              }
-            >
-              Home
+      <header className="header flex flex-col gap-6">
+        <div className="big_block_1 flex justify-between">
+          <div className="block_1">
+            <Link to="/">
+              <img
+                src={logoHeader}
+                alt=""
+                className="w-[80px] h-[80px] rounded-full"
+              />
             </Link>
-          </li>
-          <li>
-            <Link
-              to="/volunteers"
-              className={
-                location.pathname === "/volunteers"
-                  ? "dark:text-[#b6b6b6] text-[16px] font-[700] underline"
-                  : "text-[16px] font-[700] text-[#000] dark:text-[#fff] dark:hover:text-[#b6b6b6] hover:underline"
-              }
-            >
-              Volunteers
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/location"
-              className={
-                location.pathname === "/location"
-                  ? "dark:text-[#b6b6b6] text-[16px] font-[700] underline"
-                  : "text-[16px] font-[700] text-[#000] dark:text-[#fff] dark:hover:text-[#b6b6b6] hover:underline"
-              }
-            >
-              Our Location
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/about"
-              className={
-                location.pathname === "/about"
-                  ? "dark:text-[#b6b6b6] text-[16px] font-[700] underline"
-                  : "text-[16px] font-[700] text-[#000] dark:text-[#fff] dark:hover:text-[#b6b6b6] hover:underline"
-              }
-            >
-              About Us
-            </Link>
-          </li>
-        </ul>
-        <div className="block_3 flex items-center justify-end gap-3">
-          <div className="for_dark_mode sm:hidden md:block">
-            <Switcher />
           </div>
-          <select
-            name=""
-            id=""
-            className="outline-none bg-transparent text-[#333] dark:text-[#fff] sm:hidden md:block"
-          >
-            <option value="" className="dark:bg-[#000]">
-              Lang
-            </option>
-            <option value="" className="dark:bg-[#000]">
-              En
-            </option>
-            <option value="" className="dark:bg-[#000]">
-              Ru
-            </option>
-            <option value="" className="dark:bg-[#000]">
-              Tj
-            </option>
-          </select>
-          <div className="for_btn_login">
-            <button
-              className="p-[5px_40px] bg-[green] shadow-2xl text-[white] rounded-[20px] outline-none"
-              onClick={() => openModal()}
-            >
-              Login
-            </button>
-          </div>
-          <div className="btn_menu_for_mobile_size sm:block">
-            <div className="burger_menu" onClick={changeMenu}>
-              <div className={`${burger_class} bg-[#333333] dark:bg-[#fff]`}></div>
-              <div className={`${burger_class} bg-[#333333] dark:bg-[#fff]`}></div>
-              <div className={`${burger_class} bg-[#333333] dark:bg-[#fff]`}></div>
-            </div>
-          </div>
-
-          <div className={`${menu_class} text-[#fff] bg-[red] dark:bg-[green]`}>
-            <div className="navbars flex flex-col justify-center items-center gap-[10px] text-[18px] ">
-              <Link to={`/`}>Home</Link>
-              <Link to={`volunteers`}>Volunteers</Link>
-              <Link to={`location`}>Our Location</Link>
-              <Link to={`about`}>About Us</Link>
-            </div>
-            <div className="for_select_and_dark_mode flex items-center gap-[10px]">
-              <div className="for_dark_mode sm:block md:hidden">
-                <Switcher />
-              </div>
-              <select
-                name=""
-                id=""
-                className="outline-none bg-transparent text-[#333] dark:text-[#fff] sm:block md:hidden"
+          <ul className="block_2 md:flex md:items-center md:gap-5 sm:hidden">
+            <li>
+              <Link
+                to="/"
+                className={
+                  location.pathname === "/"
+                    ? " dark:text-[#b6b6b6] text-[16px] font-[700] underline"
+                    : "text-[16px] font-[700] text-[#000] dark:text-[#fff] dark:hover:text-[#b6b6b6] hover:underline"
+                }
               >
-                <option value="" className="dark:bg-[#000]">
-                  Lang
-                </option>
-                <option value="" className="dark:bg-[#000]">
-                  En
-                </option>
-                <option value="" className="dark:bg-[#000]">
-                  Ru
-                </option>
-                <option value="" className="dark:bg-[#000]">
-                  Tj
-                </option>
-              </select>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/volunteers"
+                className={
+                  location.pathname === "/volunteers"
+                    ? "dark:text-[#b6b6b6] text-[16px] font-[700] underline"
+                    : "text-[16px] font-[700] text-[#000] dark:text-[#fff] dark:hover:text-[#b6b6b6] hover:underline"
+                }
+              >
+                Volunteers
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/location"
+                className={
+                  location.pathname === "/location"
+                    ? "dark:text-[#b6b6b6] text-[16px] font-[700] underline"
+                    : "text-[16px] font-[700] text-[#000] dark:text-[#fff] dark:hover:text-[#b6b6b6] hover:underline"
+                }
+              >
+                Our Location
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/about"
+                className={
+                  location.pathname === "/about"
+                    ? "dark:text-[#b6b6b6] text-[16px] font-[700] underline"
+                    : "text-[16px] font-[700] text-[#000] dark:text-[#fff] dark:hover:text-[#b6b6b6] hover:underline"
+                }
+              >
+                About Us
+              </Link>
+            </li>
+          </ul>
+          <div className="block_3 flex items-center justify-end gap-3">
+            <div className="for_dark_mode sm:hidden md:block">
+              <Switcher />
             </div>
+            <select
+              name=""
+              id=""
+              className="outline-none bg-transparent text-[#333] dark:text-[#fff] sm:hidden md:block"
+            >
+              <option value="" className="dark:bg-[#000]">
+                Lang
+              </option>
+              <option value="" className="dark:bg-[#000]">
+                En
+              </option>
+              <option value="" className="dark:bg-[#000]">
+                Ru
+              </option>
+              <option value="" className="dark:bg-[#000]">
+                Tj
+              </option>
+            </select>
+            <div className="for_btn_login">
+              <button
+                className="p-[5px_40px] bg-[green] shadow-2xl text-[white] rounded-[20px] outline-none"
+                onClick={() => openModal()}
+              >
+                Login
+              </button>
+            </div>
+          <div className="btn_menu_for_mobile_size sm:block md:hidden">
+            <div className="burger_menu" onClick={changeMenu}>
+              <div
+                className={`${burger_class} bg-[#333333] dark:bg-[#fff]`}
+              ></div>
+              <div
+                className={`${burger_class} bg-[#333333] dark:bg-[#fff]`}
+              ></div>
+              <div
+                className={`${burger_class} bg-[#333333] dark:bg-[#fff]`}
+              ></div>
+            </div>
+          </div>
+          </div>
+        </div>
+        <div className={`${menu_class} text-[#fff] bg-[red] dark:bg-[green]`}>
+          <div className="navbars flex flex-col justify-center items-center gap-[10px] text-[18px] ">
+            <Link to={`/`}>Home</Link>
+            <Link to={`volunteers`}>Volunteers</Link>
+            <Link to={`location`}>Our Location</Link>
+            <Link to={`about`}>About Us</Link>
+          </div>
+          <div className="for_select_and_dark_mode flex items-center gap-[10px]">
+            <div className="for_dark_mode sm:block md:hidden">
+              <Switcher />
+            </div>
+            <select
+              name=""
+              id=""
+              className="outline-none bg-transparent text-[#333] dark:text-[#fff] sm:block md:hidden"
+            >
+              <option value="" className="dark:bg-[#000]">
+                Lang
+              </option>
+              <option value="" className="dark:bg-[#000]">
+                En
+              </option>
+              <option value="" className="dark:bg-[#000]">
+                Ru
+              </option>
+              <option value="" className="dark:bg-[#000]">
+                Tj
+              </option>
+            </select>
           </div>
         </div>
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,7 +6,7 @@ import "./Home.css";
 
 function Home() {
   return (
-    <div className="mt-[300px]">
+    <div>
       Home
     </div>
   );
